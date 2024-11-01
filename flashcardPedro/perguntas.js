23abd96ce496ca4ca024d51b21d623b856ba8248
pedroleon33 <161848159+pedroleon33@users.noreply.github.com>
2024-11-01 11:46:39 -0300

    Add files via upload

---
+++ flashcardPedro/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual a forma mais forte do goku?',
+    'SSJ300 + Kaioken em 1trilhão de vezes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o principal vilão de namekusei',
+    'Freeza'
+)
+
+criaCartao(
+    'Programação',
+    'piculo dourado ou CELL MAX qual melhor?',
+    'GOHAN BESTIAL'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
